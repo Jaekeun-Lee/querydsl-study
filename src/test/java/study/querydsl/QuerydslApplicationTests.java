2cb9e99a32f1268b8affc821d0bd93685289bb65
이재근 <ljg960730@etoos.com>
2022-01-24 12:35:02 +0900

    initial commit

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,45 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit
+class QuerydslApplicationTests {
+
+
+    //@PersistenceContext // 자바 표준스펙
+    @Autowired  // 스프링 주입
+    EntityManager em;
+
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertEquals(result, hello);
+        assertEquals(result.getId(), hello.getId());
+
+    }
+
+}
